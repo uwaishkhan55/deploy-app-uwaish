b6d41a7b1cd17f198fb68c911949ccf018a09277
uwaish55 <uwaishkhan55@gmail.com>
2019-03-31 17:11:02 +0530

    added some xtra stuff

--- server.js
+++ server.js
@@ -1,11 +1,39 @@
-const app=require('express')()
+const express=require('express')
+const app =express()
+var nodemailer = require('nodemailer');
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.set('view engine','hbs')
+var transporter = nodemailer.createTransport({
+    service: 'yandex',
+    auth: {
+      user: 'uwaish@yandex.com',
+      pass: 'uwaish55@#'
+    }
+  });
+  
+  var mailOptions = {
+    from: 'uwaish@yandex.com',
+    to: 'uwaishkhan55@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
+  
 app.get('/',(req,res)=>
 {
-    res.send('hello World and im  your father ')
+    res.render('index')
+})
+app.post('/sms',(req,res)=>
+{
+    console.log(req.body.forget)
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      }); 
+    res.redirect('/')
 })
 
-app.get('/hi',(req,res)=>
-{
-    res.send('good bye')
-})
-app.listen(process.env.PORT)
+app.listen(9999)
